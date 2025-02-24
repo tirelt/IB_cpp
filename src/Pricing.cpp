04214095ce66577a2773d2e197e11d52aa16fb98
theo-ssd <theotirel@gmail.com>
2025-02-24 12:40:39 +0800

    vega

--- src/Pricing.cpp
+++ src/Pricing.cpp
@@ -6,6 +6,10 @@ double normal_cdf(double x, double mean = 0.0, double stddev = 1.0) {
     return 0.5 * (1.0 + std::erf((x - mean) / (stddev * std::sqrt(2))));
 }
 
+double gaussianDensity(double x, double mean = 0.0, double stddev = 1.0) {
+    return (1.0 / (stddev * std::sqrt(2 * M_PI))) * std::exp(-0.5 * std::pow((x - mean) / stddev, 2));
+}
+
 double black_formula( Option::Right right, double forward, double strike, double vol, double time_to_maturity, double discount ){
    double temp = vol * std::sqrt( time_to_maturity); 
    double d_1 = ( std::log(forward/strike) + std::pow(temp,2) / 2 ) / temp; 
@@ -14,4 +18,8 @@ double black_formula( Option::Right right, double forward, double strike, double
     return discount*( forward * normal_cdf( d_1) - strike * normal_cdf( d_2) );
     else
     return discount*( strike * normal_cdf( -d_2) - forward * normal_cdf( -d_1) );
+}
+
+double vega( double forward, double strike, double vol, double time_to_maturity, double discount ){
+
 }
